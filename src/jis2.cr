cb0cc92c390871a9952ead9d1d217b1dd2363a1f
Emilia Dreamer <emilia@rymiel.space>
2022-01-31 00:43:06 +0200

    add helpers for array-like types for macro DSL

--- src/jis2.cr
+++ src/jis2.cr
@@ -7,7 +7,7 @@ end
 macro build_parser(kind, entrypoint, &block)
   ::Parser::Analysis(::Parser::{{ kind }}::Builder).build({{ entrypoint }}) do
     {% for i in block.body.expressions %}
-      {% if i.is_a? Assign %}
+      {% if i.is_a?(Assign) %}
         {%
           has_optional = false
           bases = [i.value]
@@ -25,46 +25,58 @@ macro build_parser(kind, entrypoint, &block)
                 elsif base.name == :|
                   bases << base.args[0]
                   base = base.receiver
+                elsif base.name == :[]
+                  call_args.unshift base.receiver
+                  has_optional = true
+                  if base.args.size > 0
+                    variants << [i.target, i.target, base.args[0], base.receiver]
+                    base = nil
+                  else
+                    base = i.target
+                  end
                 else
-                  raise base
+                  base.raise "Unknown base: #{base} (#{base.name})"
                 end
               elsif !base.is_a? NilLiteral
                 call_args.unshift base
                 base = nil
               end
             end
-            rule_args = ([i.target] + call_args).map do |j|
-              if j.is_a? Path
-                j.names[0].underscore.stringify
-              else
-                j
-              end
-            end
-            variants << rule_args
+            variants << ([i.target] + call_args)
           end
         %}
         {% if has_optional %}
-          add({{ rule_args[0] }})
+          {% target = variants[0][0] %}
+          {% target = target.is_a?(Path) ? target.names[0].underscore.stringify : target %}
+          add({{ target }})
         {% end %}
         {% for r in variants %}
+          {% r = r.map do |j|
+            if j.is_a? Path
+              j.names[0].underscore.stringify
+            else
+              j
+            end
+          end %}
           add({{ r.splat }})
         {% end %}
       {% end %}
     {% end %}
   end
+  {% debug %}
 end
 
 states = build_parser(LR0, "program") do
   Program         = Module
   Module          = :r_module + :string + Statements + :r_end
-  Statements      = optional Statements + Statement
+  Statements      = Statement[]
   Statement       = FunctionDef \
                   | :r_return + Expression + :semi \
                   | Expression + :semi \
                   | Block
   Block           = BlockStatements + :r_do + Statements + BlockFinally + :r_end
-  BlockStatements = optional BlockStatements + BlockStatement
-  BlockFinally    = optional :r_finally + Statement
+  BlockStatements = BlockStatement[]
+  BlockFinally    = optional(:r_finally + Statement)
   BlockStatement  = :r_given + Expression \
                   | :r_repeat + Expression \
                   | :r_until + Expression
@@ -74,10 +86,10 @@ states = build_parser(LR0, "program") do
   Assignment      = :word + :assign + Expression
   Call            = CallName + :sq_l + CallArgs + :sq_r
   CallName        = :word | :plus | :eq
-  CallArgs        = optional Expression | CallArgs + :pipe + Expression
+  CallArgs        = Expression[:pipe]
   TypeSpecifier   = :tt_int
   FunctionDef     = TypeSpecifier + :r_func + :word + :sq_l + DefinitionArgs + :sq_r + Statements + :r_end
-  DefinitionArgs  = optional DefinitionArg | DefinitionArgs + :pipe + DefinitionArg
+  DefinitionArgs  = DefinitionArg[:pipe]
   DefinitionArg   = TypeSpecifier + :word
 end
 at = Parser::Automaton.new(states)
