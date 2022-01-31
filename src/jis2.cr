9eb2f0fa46aa719b1b349a3d3309dd3215cadb1c
Emilia Dreamer <emilia@rymiel.space>
2022-01-31 11:22:47 +0200

    auto-deduce types of array rules

--- src/jis2.cr
+++ src/jis2.cr
@@ -4,134 +4,153 @@ def self.tok(symbol : Symbol, t : T) : {Symbol, T} forall T
   {symbol, t}
 end
 
-def desired(t : T, desired : T.class) : Any forall T
-  Any.new t
-end
-
-def desired(t, desired : T.class) : Nil forall T
-  nil
+def desired(t, desired : T.class) : Any? forall T
+  t.nil? ? nil : Any.new t
 end
 
 macro build_parser(kind, entrypoint, &block)
   {% known_types = {} of Nil => Nil %}
   ::Parser::Analysis(::Parser::{{ kind }}::Builder).build({{ entrypoint.names[0].underscore.stringify }}) do
-    {% for i in block.body.expressions %}
-      {% if i.is_a?(Assign) %}
-        {%
-          has_optional = false
-          array_base = nil
-          bases = [i.value]
-          variants = [] of Nil
-          methods = [] of Nil
-          bases.each do |base|
-            call_args = [] of Nil
-            method = nil
-            (1..50).each do # finite?
-              if base.is_a? Call
-                if base.name == :+
-                  call_args.unshift base.args[0]
-                  base = base.receiver
-                elsif base.name == :optional
-                  has_optional = true
-                  base = base.args[0]
-                elsif base.name == :|
-                  bases << base.args[0]
-                  base = base.receiver
-                elsif base.name == :[]
-                  call_args.unshift base.receiver
-                  array_base = base.receiver
-                  has_optional = true
-                  if base.args.size > 0
-                    variants << [i.target, i.target, base.args[0], base.receiver]
-                    methods << nil
-                    base = nil
-                  else
-                    base = i.target
-                  end
-                elsif base.name == :do
-                  method = base.block
-                  base = base.receiver
-                  if base.is_a?(Expressions)
-                    base = base.expressions[0]
-                  end
+    {%
+      nonterminals = [] of Nil
+      block.body.expressions.each do |i|
+        if i.is_a?(Assign)
+          nonterminals << i
+        elsif i.is_a?(Call)
+          if i.name == :type
+            j = i.args[0]
+            known_types[j.is_a?(Path) ? j.names[0].underscore.stringify : j] = i.args[1]
+          end
+        end
+      end
+      nonterminals.each do |i|
+        if i.value.is_a?(Call) && i.value.name == :[]
+          r = i.value.receiver
+          if known_receiver = known_types[r.is_a?(Path) ? r.names[0].underscore.stringify : r]
+            j = i.target
+            known_types[j.is_a?(Path) ? j.names[0].underscore.stringify : j] = parse_type("Array(#{known_receiver})")
+          end
+        end
+      end
+    %}
+    {% for i in nonterminals %}
+      {%
+        has_optional = false
+        array_base = nil
+        bases = [i.value]
+        variants = [] of Nil
+        methods = [] of Nil
+        bases.each do |base|
+          call_args = [] of Nil
+          method = nil
+          (1..50).each do # finite?
+            if base.is_a? Call
+              if base.name == :+
+                call_args.unshift base.args[0]
+                base = base.receiver
+              elsif base.name == :optional
+                has_optional = true
+                base = base.args[0]
+              elsif base.name == :|
+                bases << base.args[0]
+                base = base.receiver
+              elsif base.name == :[]
+                call_args.unshift base.receiver
+                array_base = base.receiver
+                has_optional = true
+                if base.args.size > 0
+                  variants << [i.target, i.target, base.args[0], base.receiver]
+                  methods << nil
+                  base = nil
                 else
-                  base.raise "Unknown base: #{base} (#{base.name})"
+                  base = i.target
                 end
-              elsif !base.is_a? NilLiteral
-                if base.is_a? ArrayLiteral && !base.type.nil?
-                  puts "#{base.type} #{base.type.class_name} #{base}"
-                  raise
+              elsif base.name == :do
+                method = base.block
+                base = base.receiver
+                if base.is_a?(Expressions)
+                  base = base.expressions[0]
                 end
-                call_args.unshift base
-                base = nil
+              else
+                base.raise "Unknown base: #{base} (#{base.name})"
               end
+            elsif !base.is_a? NilLiteral
+              if base.is_a? ArrayLiteral && !base.type.nil?
+                puts "#{base.type} #{base.type.class_name} #{base}"
+                raise
+              end
+              call_args.unshift base
+              base = nil
             end
-            variants << ([i.target] + call_args)
-            methods << method
+          end
+          variants << ([i.target] + call_args)
+          methods << method
+        end
+      %}
+      {% if has_optional %}
+        {% target = variants[0][0] %}
+        {% target = target.is_a?(Path) ? target.names[0].underscore.stringify : target %}
+        {% return_type = known_types[target] %}
+        {% if return_type %}
+          add({{ target }}) do
+            Any.new {{ return_type }}.new
+          end
+        {% else %}
+          add({{ target }})
+        {% end %}
+      {% end %}
+      {% for r, ri in variants %}
+        {%
+          r = r.map do |j|
+            j.is_a?(Path) ? j.names[0].underscore.stringify : j
+          end
+          m = methods[ri]
+          return_type = known_types[r[0]]
+          can_implicit_convert = false
+          if return_type && r.size == 2 && (implicit = known_types[r[1]])
+            can_implicit_convert = return_type.resolve >= implicit.resolve
           end
         %}
-        {% if has_optional %}
-          {% target = variants[0][0] %}
-          {% target = target.is_a?(Path) ? target.names[0].underscore.stringify : target %}
-          {% return_type = known_types[target] %}
-          {% if return_type %}
-            add({{ target }}) do
-              Any.new {{ return_type }}.new
-            end
-          {% else %}
-            add({{ target }})
-          {% end %}
-        {% end %}
-        {% for r, ri in variants %}
-          {%
-            r = r.map do |j|
-              j.is_a?(Path) ? j.names[0].underscore.stringify : j
-            end
-            m = methods[ri]
-            return_type = known_types[r[0]]
-            can_implicit_convert = false
-            if return_type && r.size == 2 && (implicit = known_types[r[1]])
-              can_implicit_convert = return_type.resolve >= implicit.resolve
-            end
-          %}
-          {% if m.nil? && array_base.nil? && !can_implicit_convert %}
-            add({{ r.splat }})
-          {% else %}
-            add({{ r.splat }}) do |%a|
-              {% found_indices = [] of Nil %}
-              {% for b, bi in r %}
-                {% if bi != 0 %}
-                  {% if t = known_types[b] %}
-                    %any = %a[{{ bi - 1 }}]
-                    _{{ b.id.underscore }} = _{{ bi }} = (%any.value?({{ t }})) || (puts "ResolveFailure: #{{{ bi }}} ({{ b.id }}) of {{variants[0][0]}}, found #{%any.stored_type_name} instead"; next nil)
-                    {% found_indices << bi %}
-                  {% end %}
+        {% if m.nil? && array_base.nil? && !can_implicit_convert %}
+          add({{ r.splat }})
+        {% else %}
+          add({{ r.splat }}) do |%a|
+            {% found_indices = [] of Nil %}
+            {% for b, bi in r %}
+              {% if bi != 0 %}
+                {% if t = known_types[b] %}
+                  %any = %a[{{ bi - 1 }}]
+                  _{{ b.id.underscore }} = _{{ bi }} = (%any.value?({{ t }})) || (puts "ResolveFailure: #{{{ bi }}} ({{ t }}) of {{variants[0][0]}}, found #{%any.stored_type_name} instead"; next nil)
+                  {% found_indices << bi %}
                 {% end %}
               {% end %}
-              {% if array_base %}
-                {% if found_indices.includes?(1) && found_indices.includes?(2) %}
-                  _1 << _2
-                  Any.new _1
-                {% else %}
-                  nil
-                {% end %}
-              {% elsif can_implicit_convert %}
+            {% end %}
+            {% if array_base %}
+              {% if r.size == 3 && found_indices.includes?(1) && found_indices.includes?(2) %}
+                _1 << _2
                 Any.new _1
-              {% elsif return_type %}
-                ::desired(begin
-                  {{ m.body }}
-                end, {{ return_type }})
+              {% elsif r.size == 4 && found_indices.includes?(1) && found_indices.includes?(3) %}
+                _1 << _3
+                Any.new _1
+              {% elsif r.size == 2 && return_type && found_indices.includes?(1) %}
+                Any.new {{ return_type }}{_1}
               {% else %}
-                {{ m.body }}
+                # r.size {{ r.size }}
+                # return_type {{ return_type }}
+                # found_indices {{ found_indices }}
                 nil
               {% end %}
-            end
-          {% end %}
-        {% end %}
-      {% elsif i.is_a?(Call) %}
-        {% if i.name == :type %}
-          {% j = i.args[0] %}
-          {% known_types[j.is_a?(Path) ? j.names[0].underscore.stringify : j] = i.args[1] %}
+            {% elsif can_implicit_convert %}
+              Any.new _1
+            {% elsif return_type %}
+              ::desired(begin
+                {{ m.body }}
+              end, {{ return_type }})
+            {% else %}
+              {{ m.body }}
+              nil
+            {% end %}
+          end
         {% end %}
       {% end %}
     {% end %}
@@ -139,25 +158,55 @@ macro build_parser(kind, entrypoint, &block)
   {% debug %}
 end
 
+class Object
+  def ref : JIS2::Ref(self)
+    JIS2::Ref.new self
+  end
+end
+
 module JIS2
   enum PrimitiveType
     Integer
   end
+  enum BlockStatementKind
+    Given
+    Repeat
+    Until
+  end
   alias Type = PrimitiveType
+  record TypeName, type : Type, name : String
+
+  struct Ref(T)
+    @t : Slice(T)
+    def initialize(t : T)
+      @t = Slice(T).new(1, t)
+    end
+
+    def value : T
+      @t[0]
+    end
+
+    def to_s(io : IO)
+      value.to_s io
+    end
+
+    def inspect(io : IO)
+      io << "&"
+      value.inspect io
+    end
+  end
 
   abstract class Statement
   end
-  abstract class Expression
+  abstract struct Expression
   end
-  class Declaration < Expression
-    def initialize(@type : TypeName, @value : Expression)
-    end
+  abstract struct BlockStatement
   end
-  class DecimalLiteral < Expression
-    def initialize(@value : Int64)
-    end
-  end
-  record TypeName, type : Type, name : String
+  record ExpressionBlockStatement < BlockStatement, kind : BlockStatementKind, value : Expression
+
+  record Declaration < Expression, type : TypeName, value : Ref(Expression)
+  record DecimalLiteral < Expression, value : Int64
+
   class Module
     def initialize(@name : String)
     end
@@ -171,15 +220,14 @@ states = build_parser(LR1, Program) do
   type TypeSpecifier, JIS2::Type
   type Program,       JIS2::Module
   type Module,        JIS2::Module
-  type Statements,    Array(JIS2::Statement)
   type Statement,     JIS2::Statement
-  type Expressions,    Array(JIS2::Expression)
   type Expression,     JIS2::Expression
   type DefinitionArg, JIS2::TypeName
+  type BlockStatement, JIS2::BlockStatement
   type DecimalLiteral, JIS2::DecimalLiteral
   type Declaration, JIS2::Declaration
 
-  Program         = Module.do { _module }
+  Program         = Module
   Module          = (:r_module + :string + Statements + :r_end).do { JIS2::Module.new _string }
   Statements      = Statement[]
   Statement       = FunctionDef \
@@ -189,12 +237,12 @@ states = build_parser(LR1, Program) do
   Block           = BlockStatements + :r_do + Statements + BlockFinally + :r_end
   BlockStatements = BlockStatement[]
   BlockFinally    = optional(:r_finally + Statement)
-  BlockStatement  = :r_given + Expression \
-                  | :r_repeat + Expression \
-                  | :r_until + Expression
+  BlockStatement  = (:r_given + Expression).do { JIS2::ExpressionBlockStatement.new(:given, _2) } \
+                  | (:r_repeat + Expression).do { JIS2::ExpressionBlockStatement.new(:repeat, _2) } \
+                  | (:r_until + Expression).do { JIS2::ExpressionBlockStatement.new(:until, _2) }
   Expression      = DecimalLiteral | :word | Declaration | Assignment | Call
   DecimalLiteral  = :number.do { JIS2::DecimalLiteral.new _number }
-  Declaration     = (DefinitionArg + :assign + Expression).do { JIS2::Declaration.new _definition_arg, _expression }
+  Declaration     = (DefinitionArg + :assign + Expression).do { JIS2::Declaration.new _definition_arg, _expression.ref }
   Assignment      = :word + :assign + Expression
   Call            = CallName + :sq_l + CallArgs + :sq_r
   CallName        = :word | :plus | :eq
@@ -204,6 +252,7 @@ states = build_parser(LR1, Program) do
   DefinitionArgs  = DefinitionArg[:pipe]
   DefinitionArg   = (TypeSpecifier + :word).do { JIS2::TypeName.new _type_specifier, _word }
 end
+pp! states.size
 at = Parser::Automaton.new(states)
 
 class TreeWalker(*T)
@@ -259,5 +308,5 @@ at << :r_module << tok(:string, "default")
   at << :r_end
 at << :r_end
 tree = at.run
-PrettyPrint.format(TreeWalker(Array(JIS2::Statement), JIS2::TypeName, JIS2::Declaration).new.walk(tree), STDOUT, 119)
+PrettyPrint.format(TreeWalker(Array(JIS2::Statement), Array(JIS2::TypeName), Array(JIS2::BlockStatement), JIS2::TypeName, JIS2::Module, String, JIS2::PrimitiveType).new.walk(tree), STDOUT, 119)
 puts
